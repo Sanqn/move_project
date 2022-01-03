f65e11c95e2a1febd2ed18adaa9393191f00675b
Sanqn <sandisvert@gmail.com>
2022-01-03 22:08:11 +0300

    django_edit_save_delete

--- movie_app/models.py
+++ movie_app/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class Models(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    sum_money = models.IntegerField(default=1000000)
+
+    def __str__(self):
+        return f'{self.name} - {self.rating}%'
