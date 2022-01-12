ec80dbb171b17df0c99e4a9d348d914c8688eedb
Sanqn <sandisvert@gmail.com>
2022-01-12 22:03:41 +0300

    django_aggregation

--- movie_app/views.py
+++ movie_app/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
-
 from .models import Models
+from django.db.models import F, Sum, Max, Min, Count, Avg
+
 def index(request):
-    movies = Models.objects.order_by('-rating', '-sum_money') # sort by field order_by('rating', 'sum_money')
+    movies = Models.objects.all() # sort by field order_by('rating', 'sum_money')
     # for movie in movies: #заполняем в бд колонку slug
     #     movie.save()
+    mov = movies.aggregate(Avg('sum_money'), Count('name'))
     context = {
         'hello': 'List of movies',
         'movies': movies,
+        'mov': mov,
     }
     return render(request, 'movie_app/index.html', context=context)
 
